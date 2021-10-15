8ba84c7e2405a8221cbf99dd8cad371563c45b15
nurbek <abduhamidovnurbek888@gmail.com>
2021-10-15 18:23:38 +0500

    no commit

--- src/components/Modal/Modul.jsx
+++ src/components/Modal/Modul.jsx
@@ -4,8 +4,8 @@ import "./modul.css";
 
 function Modul() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [accessNumber, setAccessNumber] = useState("");
-  const [number, setNumber] = useState("");
+  const [accessNumber,setAccessNumber] = useState("")
+  const [number,setNumber] = useState("");
 
   return (
     <div className="modal">
@@ -30,37 +30,35 @@ function Modul() {
         <div className="inModalForm">
           <form className="modalForm">
             <p>Telefon raqam</p>
-            <input
-              type="text"
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-              className="modalInput"
-              placeholder="+998 ( )"
-            />
+            <input type="text"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+             className="modalInput" 
+             placeholder="+998 ( )" />
           </form>
         </div>
         <div className="inModalForm">
           <form className="modalForm">
             <p>Parol</p>
-            <input type="password" className="modalInput" />
+            <input type="password"
+             className="modalInput" 
+              />
           </form>
-          <div className="mainCheckbox">
-            <input type="checkbox" className="checkbox" />
-            <p>Eslab qolish</p>
-          </div>
-          <div className="warning">Parolni kiriting kamida 8ta belgi</div>
-          <div className="enterBtn">
-            <button
-              className="btn"
-              onClick={() => {
-                setAccessNumber(number);
-              }}
-            >
-              Kirish
-            </button>
-            <span className="createParol">Parolni tiklash</span>
-            <span className="register">Ro`yhatdan o`tish</span>
-          </div>
+        <div className="mainCheckbox">
+          <input type="checkbox" 
+          className="checkbox"
+          />
+          <p>Eslab qolish</p>
+        </div>
+        <div className="warning">Parolni kiriting kamida 8ta belgi</div>
+        <div className="enterBtn">
+          <button className="btn"
+          onClick={() => {setAccessNumber(number)}}
+          >Kirish</button>
+          <span className="createParol">Parolni tiklash</span>
+          <span className="register">Ro`yhatdan o`tish</span>
+          <p>{accessNumber}</p>
+        </div>
         </div>
       </Modal>
     </div>
