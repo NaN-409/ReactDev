5b021ae18abfa524eef5b567ddc603ea72ae4cc2
nurbek <abduhamidovnurbek888@gmail.com>
2021-10-16 10:36:42 +0500

    modulga raqam kiritish

--- src/components/Modal/Modul.jsx
+++ src/components/Modal/Modul.jsx
@@ -4,8 +4,37 @@ import "./modul.css";
 
 function Modul() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [accessNumber,setAccessNumber] = useState("")
-  const [number,setNumber] = useState("");
+  const [accessNumber, setAccessNumber] = useState("");
+  const [number, setNumber] = useState("");
+  const [error, setError] = useState("");
+  const p = "123456789";
+  const nurbek = "5555";
+  const kamol = "4444";
+  const shahboz = "3333";
+  const nothing = "";
+  const border = 13;
+
+  const [accept, setAccept] = useState("");
+
+  const onSubmit = () => {
+    if (accept === nurbek) {
+      setAccept("Nurbekni nomeri");
+    } else {
+      setAccept("Bunday nomer mavjud emas");
+    }
+    if (accept === kamol) {
+      setAccept("kamolni nomeri");
+    }
+    if (accept === shahboz) {
+      setAccept("Shahbozni nomeri");
+    }
+    if (accept === nothing) {
+      setAccept("Nomerni to'liq kiriting");
+    }
+    if (nothing >= border) {
+      setAccept("Siz haddan tashqari ko`p raqam kiritdingiz")
+    }
+  };
 
   return (
     <div className="modal">
@@ -30,35 +59,42 @@ function Modul() {
         <div className="inModalForm">
           <form className="modalForm">
             <p>Telefon raqam</p>
-            <input type="text"
-            value={number}
-            onChange={(e) => setNumber(e.target.value)}
-             className="modalInput" 
-             placeholder="+998 ( )" />
+            <input
+              type="text"
+              onChange={(e) => setAccept(e.target.value)}
+              className="modalInput"
+              placeholder="+998 ( )"
+            />
+            <p className="warning">{accept}</p>
           </form>
         </div>
         <div className="inModalForm">
           <form className="modalForm">
             <p>Parol</p>
-            <input type="password"
-             className="modalInput" 
-              />
+            <input
+              type="password"
+              // onChange={(e) => setPass(e.target.value)}
+              className="modalInput"
+            />
           </form>
-        <div className="mainCheckbox">
-          <input type="checkbox" 
-          className="checkbox"
-          />
-          <p>Eslab qolish</p>
-        </div>
-        <div className="warning">Parolni kiriting kamida 8ta belgi</div>
-        <div className="enterBtn">
-          <button className="btn"
-          onClick={() => {setAccessNumber(number)}}
-          >Kirish</button>
-          <span className="createParol">Parolni tiklash</span>
-          <span className="register">Ro`yhatdan o`tish</span>
-          <p>{accessNumber}</p>
-        </div>
+          <div className="mainCheckbox">
+            <input type="checkbox" className="checkbox" />
+            <p>Eslab qolish</p>
+          </div>
+          <div className="warning">{error}</div>
+          <div className="enterBtn">
+            <button
+              className="btn"
+              onClick={() => {
+                onSubmit();
+              }}
+            >
+              Kirish
+            </button>
+            <span className="createParol">Parolni tiklash</span>
+            <span className="register">Royhatdan o`tish</span>
+            <p>{accessNumber}</p>
+          </div>
         </div>
       </Modal>
     </div>
